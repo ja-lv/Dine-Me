48d0d24faed1bc009bc93ff3776f7a9daa384b8c
Jeffry Alvarado <alvaradojeffry95@gmail.com>
2018-12-11 16:21:17 -0800

    repo updates

--- app/src/main/java/com/me/dine/dineme/ViewModel/DineMeRepository.java
+++ app/src/main/java/com/me/dine/dineme/ViewModel/DineMeRepository.java
@@ -1,7 +1,27 @@
 package com.me.dine.dineme.ViewModel;
 
-public class DineMeRepository {
-    public DineMeRepository(Application application){
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+import android.util.Log;
 
+import com.me.dine.dineme.ViewModel.LocalDatabase.DAO.MainUserDao;
+import com.me.dine.dineme.ViewModel.LocalDatabase.DAO.RGroupsDao;
+import com.me.dine.dineme.ViewModel.LocalDatabase.DBClasses.DineMeMainUser;
+import com.me.dine.dineme.ViewModel.LocalDatabase.DBClasses.DineMeRGroup;
+import com.me.dine.dineme.ViewModel.LocalDatabase.DineMeRoomDB;
+
+import java.util.List;
+
+public class DineMeRepository {
+    private MainUserDao mMainUserDao;
+    private RGroupsDao mRGroupsDao;
+    private LiveData<DineMeMainUser> mMainUser;
+    private LiveData<List<DineMeRGroup>> mDineMeRGroups;
+
+    public DineMeRepository(Application application){
+        DineMeRoomDB db = DineMeRoomDB.getInstance(application.getApplicationContext());
+        mMainUserDao = db.mainUserDao();
+        mRGroupsDao = db.rGroupsDao();
     }
 }
